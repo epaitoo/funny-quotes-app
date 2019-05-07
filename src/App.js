1a16c8d22fad8daa1b614698cd1687181a86903b
Eugene Paitoo <eugenepaitoo@gmail.com>
2019-05-07 18:09:13 +0000

    change bg color

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { quotes } from './data/quotes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+
+  state = {
+    quotes,
+    randomQuote : null,
+    isClicked : false
+  }
+
+
+  showRandomQuote = () => {
+    const { quotes } = this.state;
+  
+    this.setState({ 
+        randomQuote : quotes[Math.floor(Math.random() * quotes.length)],
+        isClicked : true
+     })
+  }
+
+  render() {
+    
+    const { randomQuote, isClicked } = this.state;
+
+    return(
+      <div className="container">
+          { isClicked ? 
+            (<Quote 
+              randomItem = {randomQuote}
+            />) 
+            : 
+            (<p className="info">Funny Quotes At random...</p>)
+          }
+        <button className="loadQuote" onClick={this.showRandomQuote}>Load Quote</button>
+      </div>
+      
+    );
+  }
+}
+
+const Quote = ({ randomItem }) => {
+  return(
+    <div className="quote-box" key={randomItem.id}>
+      <p className="quote">{randomItem.quote}</p>
+      <p className="source">{randomItem.source}</p>    
     </div>
   );
 }
---
+++ src/data/quotes.js
@@ -0,0 +1,58 @@
+export const quotes = [
+    {
+      id: 0,
+      quote: "When you are stressed you eat ice cream, cake, chocolate and sweets, because stressed spelled backwards is desserts.",
+      source: "Anonymous"
+    },
+    {
+      id: 1,
+      quote: "All my bills say 'Outstanding'. I guess I am good to go",
+      source: "Anonymous"
+    },
+    {
+      id: 2,
+      quote: "They say that love is more important than money, but have you ever tried to pay your bills with a hug?",
+      source: "Anonymous"
+    },
+    {
+      id: 3,
+      quote: "If you think nobody cares if you're alive, try missing a couple of payments",
+      source: "Anonymous",
+    },
+    {
+      id: 4,
+      quote: "An apple a day keeps anyone away if you throw it hard enough.",
+      source: "Anonymous"
+    },
+    {
+      id: 5,
+      quote: "I remixed a remix, it was back to normal.",
+      source: "Mitch Hedberg"
+    },
+    {
+      id: 6,
+      quote: "Never trust people who smile constantly. They’re either selling something or not very bright",
+      source: "Laurell K. Hamilton"
+    },
+    {
+      id: 7,
+      quote: "Everything is changing. People are taking the comedians seriously and the politicians as a joke.",
+      source: "Will Rogers"
+    },
+    {
+      id: 8,
+      quote: "The difference between stupidity and genius is that genius has its limits.",
+      source: "Albert Einstein"
+    },
+    {
+      id: 9,
+      quote: "What do people do with all the extra time they save by writing ‘k’ instead of ‘ok’?",
+      source: "Anonymous"
+    },
+    {
+      id: 10,
+      quote: "If we’re not meant to have midnight snacks, why is there a light in the fridge.",
+      source: "Anonymous"
+    },
+  ];
+  
